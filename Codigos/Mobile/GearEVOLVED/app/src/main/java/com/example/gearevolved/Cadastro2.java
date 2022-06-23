c3c76ad5e34d86b039a76dd012e6e03147e875b4
Vinícius P. Petto <vinicius.petto.8@hotmail.com>
2022-06-23 08:20:19 -0300

    Criação de uma nova tela de mobile mais modificação desktop

---
+++ Codigos/Mobile/GearEVOLVED/app/src/main/java/com/example/gearevolved/Cadastro2.java
@@ -0,0 +1,14 @@
+package com.example.gearevolved;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro2);
+    }
+}
